3179f9bab3e769931a009d57d52c90cf0885ecec
Nakayama Daiju <42.daiju@gmail.com>
2020-12-28 12:17:19 +0900

    sorting network 2,3

---
+++ src/sorting_network.rs
@@ -0,0 +1,55 @@
+unsafe fn sort_2<T: PartialOrd + Copy>(a: *mut T, b: *mut T) {
+    let min_v = if *a < *b { *a } else { *b };
+    let max_v = if *a >= *b { *a } else { *b };
+    *a = min_v;
+    *b = max_v;
+}
+
+pub fn sort2<T: PartialOrd + Copy>(arr: &mut [T; 2]) {
+    unsafe {
+        sort_2(&mut arr[0], &mut arr[1]);
+    }
+}
+
+pub fn sort3<T: PartialOrd + Copy>(arr: &mut [T; 3]) {
+    unsafe {
+        sort_2(&mut arr[0], &mut arr[1]);
+        sort_2(&mut arr[0], &mut arr[2]);
+        sort_2(&mut arr[1], &mut arr[2]);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn check_sorted(arr: &[i32]) {
+        let mut v = arr.to_vec();
+        v.sort();
+        assert_eq!(arr, v.as_slice());
+    }
+
+    #[test]
+    fn test_sort2() {
+        for a0 in 0..=1 {
+            for a1 in 0..=1 {
+                let mut arr = [a0, a1];
+                sort2(&mut arr);
+                check_sorted(&arr);
+            }
+        }
+    }
+
+    #[test]
+    fn test_sort3() {
+        for a0 in 0..=1 {
+            for a1 in 0..=1 {
+                for a2 in 0..=1 {
+                    let mut arr = [a0, a1, a2];
+                    sort3(&mut arr);
+                    check_sorted(&arr);
+                }
+            }
+        }
+    }
+}
